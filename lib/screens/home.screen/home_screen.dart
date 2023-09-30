eb46b491495d31baec321ecb19980a8c2d40abcb
Avinash174 <avinashmagar15@gmail.com>
2023-09-30 17:37:40 +0530

    new changes done

--- lib/screens/home.screen/home_screen.dart
+++ lib/screens/home.screen/home_screen.dart
@@ -1,32 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_try/screens/login.screen/login_view_model.dart';
 import 'package:mvvm_try/utils/utils.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            children: [
-              Center(
-                child: InkWell(
-                  onTap: () {
-                    Utils.snackBar('Hello', context);
-                  },
-                  child: const Text('Log In'),
-                ),
+          Center(
+            child: InkWell(
+              onTap: () {
+                Utils.snackBar('Hello', context);
+              },
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Press'),
               ),
-            ],
+            ),
           ),
         ],
       ),
